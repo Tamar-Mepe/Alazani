134726ea78bf148e3bcf12ce48bf3743b6a25068
irakli <igabe17@freeuni.edu.ge>
2020-07-08 22:18:41 +0400

    init connection

--- src/main/java/models/db/DB.java
+++ src/main/java/models/db/DB.java
@@ -1,5 +1,9 @@
 package models.db;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public interface DB {
     String getAll(String tableName);
+    Connection connectToDatabase() throws ClassNotFoundException, SQLException;
 }
--- src/main/java/models/db/MySQL.java
+++ src/main/java/models/db/MySQL.java
@@ -1,8 +1,28 @@
 package models.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MySQL implements DB {
+    private Connection conn;
     @Override
     public String getAll(String tableName) {
         return "SELECT * FROM " + tableName + ";";
     }
+
+    @Override
+    public Connection connectToDatabase() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection currCon = DriverManager.getConnection(Environment.URL,Environment.USER,Environment.PASSWORD);
+        Statement useStatement =  conn.createStatement();
+        useStatement.execute( "USE "  + Environment.DATABASE +";");
+        return currCon;
+    }
+
+    public MySQL() throws SQLException, ClassNotFoundException {
+        conn = connectToDatabase();
+    }
+
 }
