cb46d094a197d90377db8583e6aafdf00c52f206
tamar-mepe <tdavi18@freeuni.edu.ge>
2020-07-08 21:57:30 +0400

    add environment

---
+++ src/main/java/models/db/Environment.java
@@ -0,0 +1,8 @@
+package models.db;
+
+public class Environment {
+    public static final String URL = "jdbc:mysql://localhost";
+    public static final String USER = "";
+    public static final String PASSWORD = "";
+    public static final String DATABASE = "";
+}
