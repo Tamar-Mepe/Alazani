21033dc9b0c0193e9b7e8f33b28aef1863c77bef
tamar-mepe <tdavi18@freeuni.edu.ge>
2020-07-26 20:52:28 +0400

    keeping things professional

--- src/main/java/db/seeder/ProductSeeder.java
+++ src/main/java/db/seeder/ProductSeeder.java
@@ -29,29 +29,28 @@ public class ProductSeeder {
         }
         Random rand = new Random();
         // seed
-            new Product("iPhone 11 Pro Max", "gacvlac mawyobs tirkmelshi", 999, categoryId.get("Electronics"), 2, userId.get(rand.nextInt(userId.size())), "images/product/iphone.jpeg").save();
-            new Product("Yoda Figure", "buds all green like yoda", 25, categoryId.get("Toys"), 16, userId.get(rand.nextInt(userId.size())), "images/product/yoda.png").save();
-            new Product("Laptop", "macbook pro desit tishi laria micemuli prosta mechqareba da iafad vyidi", 3000, categoryId.get("Electronics"), 3, userId.get(rand.nextInt(userId.size())), "images/product/laptop.png").save();
-            new Product("Converse", "it does not need description", 999, categoryId.get("Shoes"), 3, userId.get(rand.nextInt(userId.size())), "images/product/converse.png").save();
-            new Product("Toaster", "tosts bread or other identical things", 999, categoryId.get("Kitchen"), 3, userId.get(rand.nextInt(userId.size())), "images/product/toaster.jpg").save();
-            new Product("Owl", "toy owl with ability to kiss", 69, categoryId.get("Toys"), 3, userId.get(rand.nextInt(userId.size())), "images/product/owl.png").save();
-            new Product("Hood", "creepy owl hood", 45, categoryId.get("Clothes"), 6, userId.get(rand.nextInt(userId.size())), "images/product/hood.jpg").save();
-            new Product("Keyboard", "gaming keyboard", 13, categoryId.get("Gaming"), 1, userId.get(rand.nextInt(userId.size())), "images/product/keyboard.jpg").save();
-            new Product("Airpods", "wireless headphones", 11, categoryId.get("Phone Accessories"), 13, userId.get(rand.nextInt(userId.size())), "images/product/airpod.jpeg").save();
-            new Product("Owl Case", "case with ability to kiss", 13, categoryId.get("Phone Accessories"), 1, userId.get(rand.nextInt(userId.size())), "images/product/case1.jpg").save();
+            new Product("iPhone 11 Pro Max", "Gacvlac mawyobs tirkmelshi", 999, categoryId.get("Electronics"), 2, userId.get(rand.nextInt(userId.size())), "images/product/iphone.jpeg").save();
+            new Product("Yoda Figure", "Buds all green like yoda", 25, categoryId.get("Toys"), 16, userId.get(rand.nextInt(userId.size())), "images/product/yoda.png").save();
+            new Product("Laptop", "MacBook Pro desit tishi laria micemuli prosta mechqareba da iafad vyidi", 3000, categoryId.get("Electronics"), 3, userId.get(rand.nextInt(userId.size())), "images/product/laptop.png").save();
+            new Product("Converse", "It does not need description", 999, categoryId.get("Shoes"), 3, userId.get(rand.nextInt(userId.size())), "images/product/converse.png").save();
+            new Product("Toaster", "Toasts bread or other identical things", 999, categoryId.get("Kitchen"), 3, userId.get(rand.nextInt(userId.size())), "images/product/toaster.jpg").save();
+            new Product("Owl", "Toy owl with ability to kiss", 69, categoryId.get("Toys"), 3, userId.get(rand.nextInt(userId.size())), "images/product/owl.png").save();
+            new Product("Hood", "Creepy owl hood", 45, categoryId.get("Clothes"), 6, userId.get(rand.nextInt(userId.size())), "images/product/hood.jpg").save();
+            new Product("Keyboard", "Gaming keyboard", 13, categoryId.get("Gaming"), 1, userId.get(rand.nextInt(userId.size())), "images/product/keyboard.jpg").save();
+            new Product("Airpods", "Wireless headphones", 11, categoryId.get("Phone Accessories"), 13, userId.get(rand.nextInt(userId.size())), "images/product/airpod.jpeg").save();
+            new Product("Owl Case", "Case with ability to kiss", 13, categoryId.get("Phone Accessories"), 1, userId.get(rand.nextInt(userId.size())), "images/product/case1.jpg").save();
             new Product("Busu", "Bu with ability to su", 13, categoryId.get("Phone Accessories"), 1, userId.get(rand.nextInt(userId.size())), "images/product/case2.jpg").save();
-            new Product("Ass case", "Kiss me owl", 13, categoryId.get("Phone Accessories"), 1, userId.get(rand.nextInt(userId.size())), "images/product/case3.jfif").save();
             new Product("Headset", "Gaming headset", 13, categoryId.get("Gaming"), 1, userId.get(rand.nextInt(userId.size())), "images/product/headset.jpg").save();
             new Product("Mousepad", "Gaming mousepad", 13, categoryId.get("Gaming"), 1, userId.get(rand.nextInt(userId.size())), "images/product/mousepad.jpg").save();
             new Product("Mouse", "Gaming mouse", 13, categoryId.get("Gaming"), 1, userId.get(rand.nextInt(userId.size())), "images/product/mouse.jpg").save();
             new Product("Necklace", "Owl necklace", 13, categoryId.get("Accessories"), 1, userId.get(rand.nextInt(userId.size())), "images/product/necklace.jpg").save();
-            new Product("Phone charger", "Wired phone charger", 13, categoryId.get("Phone Accessories"), 1, userId.get(rand.nextInt(userId.size())), "images/product/phone charger.jpg").save();
-            new Product("Sapilpilee da Samarile", "Salt and Pepper shakers", 13, categoryId.get("Kitchen"), 1, userId.get(rand.nextInt(userId.size())), "images/product/salt&pepper shaker.jpg").save();
-            new Product("Shoe", "owl shoes", 23, categoryId.get("Shoes"), 5, userId.get(rand.nextInt(userId.size())), "images/product/shoes.jpg").save();
-            new Product("Sunglasses", "armenian sunglasses", 223, categoryId.get("Accessories"), 1, userId.get(rand.nextInt(userId.size())), "images/product/sunglasses.jpg").save();
-            new Product("Swimsuit", "boobs are blessing u and this swimsuit blesses boobs", 223, categoryId.get("Clothes"), 1, userId.get(rand.nextInt(userId.size())), "images/product/swim suit.jpg").save();
-            new Product("Owl talisman", "no words needed", 11, categoryId.get("Accessories"), 3, userId.get(rand.nextInt(userId.size())), "images/product/talisman.jpg").save();
-            new Product("Wireless charger", "tamaram traki washaleto", 9, categoryId.get("Phone Accessories"), 3, userId.get(rand.nextInt(userId.size())), "images/product/wirelessCharger.jpg").save();
+            new Product("Phone Charger", "Wired phone charger", 13, categoryId.get("Phone Accessories"), 1, userId.get(rand.nextInt(userId.size())), "images/product/phone charger.jpg").save();
+            new Product("Sapilpilee Da Samarile", "Salt and Pepper shakers", 13, categoryId.get("Kitchen"), 1, userId.get(rand.nextInt(userId.size())), "images/product/salt&pepper shaker.jpg").save();
+            new Product("Shoes", "Owl shoes", 23, categoryId.get("Shoes"), 5, userId.get(rand.nextInt(userId.size())), "images/product/shoes.jpg").save();
+            new Product("Sunglasses", "Armenian sunglasses", 223, categoryId.get("Accessories"), 1, userId.get(rand.nextInt(userId.size())), "images/product/sunglasses.jpg").save();
+            new Product("Swimsuit", "Great swimsuit", 223, categoryId.get("Clothes"), 1, userId.get(rand.nextInt(userId.size())), "images/product/swim suit.jpg").save();
+            new Product("Owl Talisman", "No words needed", 11, categoryId.get("Accessories"), 3, userId.get(rand.nextInt(userId.size())), "images/product/talisman.jpg").save();
+            new Product("Wireless Charger", "Great charger", 9, categoryId.get("Phone Accessories"), 3, userId.get(rand.nextInt(userId.size())), "images/product/wirelessCharger.jpg").save();
 
     }
 }
