316f637efd424097b8259721e16526c53b7c3a5c
David Targamadze <dtarg16@freeuni.edu.ge>
2020-07-27 13:15:42 +0400

    add bcrypt class and unit tests

---
+++ src/main/java/tests/BCryptTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import utils.BCrypt;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BCryptTest {
+    private String passwords[] = {"Jij1*!@)(#naaaaaaaaAskkkasdllll",
+            "RuNHIj5BK236TC",
+            "StillDoggOOP_1",
+            "MeArviYoParoliTu"};
+    @Test
+    void simpleTest() throws SQLException {
+        String password ="Oesischabareba";
+        String savedPassword ="Oesischabareba";
+        String generatedSecuredPasswordHash = BCrypt.hashpw(password, BCrypt.gensalt(12));
+        assertTrue(BCrypt.checkpw(savedPassword, generatedSecuredPasswordHash));
+
+    }
+    @Test
+    void mediocreTest() throws SQLException {
+        List<String> hashedPasswords = new ArrayList<String>();
+        for(int i=0; i<passwords.length; i++){
+            String generatedSecuredPasswordHash = BCrypt.hashpw(passwords[i], BCrypt.gensalt());
+            hashedPasswords.add(generatedSecuredPasswordHash);
+        }
+
+        for(int i=0; i<10; i++){
+            for(int j=0; j<passwords.length; j++){
+                assertTrue(BCrypt.checkpw(passwords[j], hashedPasswords.get(j)));
+                String almostSamePassword = passwords[j]+" ";
+                assertFalse(BCrypt.checkpw(almostSamePassword, hashedPasswords.get(j)));
+
+            }
+        }
+    }
+
+}
